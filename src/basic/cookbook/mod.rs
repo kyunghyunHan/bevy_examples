c62ed4d896ddd6849cd9a2a4a3fc6096f2e1275e
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-10-17 22:38:30 +0900

    feat:entity

--- src/basic/cookbook/mod.rs
+++ src/basic/cookbook/mod.rs
@@ -1,2 +1,3 @@
 pub mod frame;
 pub mod _2d_game;
+pub mod entity;
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use bevy_examples::{background, basic, character, keyboard, moving, network, phy
 fn main() {
     // basic::cookbook::frame::example();
     basic::cookbook::_2d_game::example();
+    basic::cookbook::entity::example();
     // physics::basic::ball_collisions::example();
     // character::example();
     // network::udp::example();
